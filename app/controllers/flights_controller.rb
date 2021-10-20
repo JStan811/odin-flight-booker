d192062497394252e401896d81082e31affa4119
Jake Standley <jakestandley95@gmail.com>
2021-10-20 18:18:46 -0500

    Finished first section - 'Screen 1: Search'

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,6 @@
 class FlightsController < ApplicationController
   def index
     @flights = Flight.all
+    @uniq_flights_sorted = Flight.pluck(:start_datetime).map {|a| [a.to_s[0, 10], a.to_s[0, 10]] }.uniq.sort
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
-# Clear out all tables before repopulating them
+# Clear out all tables and reset pk sequence before repopulating them
 Airport.delete_all
+ActiveRecord::Base.connection.execute("UPDATE SQLITE_SEQUENCE SET SEQ=0 WHERE NAME='airports';")
 Flight.delete_all
+ActiveRecord::Base.connection.execute("UPDATE SQLITE_SEQUENCE SET SEQ=0 WHERE NAME='flights';")
 
 Airport.create(code: 'SFO')
 Airport.create(code: 'NYC')
@@ -30,6 +31,14 @@ Flight.create(start_airport_id: Airport.find_by(code: 'SFO').id, finish_airport_
 
 Flight.create(start_airport_id: Airport.find_by(code: 'SFO').id, finish_airport_id: Airport.find_by(code: 'NYC').id, start_datetime: '2021-10-22 14:30', duration: 330)
 
+Flight.create(start_airport_id: Airport.find_by(code: 'SFO').id, finish_airport_id: Airport.find_by(code: 'NYC').id, start_datetime: '2021-11-15 12:30', duration: 330)
+
+Flight.create(start_airport_id: Airport.find_by(code: 'SFO').id, finish_airport_id: Airport.find_by(code: 'NYC').id, start_datetime: '2021-10-30 10:30', duration: 330)
+
+Flight.create(start_airport_id: Airport.find_by(code: 'SFO').id, finish_airport_id: Airport.find_by(code: 'NYC').id, start_datetime: '2021-12-01 10:00', duration: 330)
+
+Flight.create(start_airport_id: Airport.find_by(code: 'SFO').id, finish_airport_id: Airport.find_by(code: 'NYC').id, start_datetime: '2021-11-29 11:00', duration: 330)
+
 Flight.create(start_airport_id: Airport.find_by(code: 'NYC').id, finish_airport_id: Airport.find_by(code: 'SFO').id, start_datetime: '2021-10-20 14:00', duration: 330)
 
 Flight.create(start_airport_id: Airport.find_by(code: 'NYC').id, finish_airport_id: Airport.find_by(code: 'SFO').id, start_datetime: '2021-10-20 22:00', duration: 330)
@@ -37,3 +46,11 @@ Flight.create(start_airport_id: Airport.find_by(code: 'NYC').id, finish_airport_
 Flight.create(start_airport_id: Airport.find_by(code: 'NYC').id, finish_airport_id: Airport.find_by(code: 'SFO').id, start_datetime: '2021-10-22 10:00', duration: 330)
 
 Flight.create(start_airport_id: Airport.find_by(code: 'NYC').id, finish_airport_id: Airport.find_by(code: 'SFO').id, start_datetime: '2021-10-22 6:00', duration: 330)
+
+Flight.create(start_airport_id: Airport.find_by(code: 'NYC').id, finish_airport_id: Airport.find_by(code: 'SFO').id, start_datetime: '2021-10-29 10:00', duration: 330)
+
+Flight.create(start_airport_id: Airport.find_by(code: 'NYC').id, finish_airport_id: Airport.find_by(code: 'SFO').id, start_datetime: '2021-10-25 6:00', duration: 330)
+
+Flight.create(start_airport_id: Airport.find_by(code: 'NYC').id, finish_airport_id: Airport.find_by(code: 'SFO').id, start_datetime: '2021-11-25 8:00', duration: 330)
+
+Flight.create(start_airport_id: Airport.find_by(code: 'NYC').id, finish_airport_id: Airport.find_by(code: 'SFO').id, start_datetime: '2021-11-01 4:00', duration: 330)
