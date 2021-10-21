76209ee8fb6685797fe3006f97f0dbb007e16f8c
Jake Standley <jakestandley95@gmail.com>
2021-10-20 19:06:35 -0500

    Finish available flights radio button. Section 2 step 4 of the project.

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,6 +3,6 @@ class Flight < ApplicationRecord
   belongs_to :to_airport, foreign_key: 'finish_airport_id', class_name: 'Airport'
 
   def start_datetime_formatted
-    start_datetime.in_time_zone("Central Time (US & Canada)").strftime("%D")
+    start_datetime.in_time_zone("Central Time (US & Canada)").strftime("%c")
   end
 end
